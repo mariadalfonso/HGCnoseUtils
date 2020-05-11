d119cdedb6edff0d6c89461b379a8995dc2193d9
maria <dalfonso@cern.ch>
2020-05-12 00:33:59 +0200

    cleanup config analysis

--- Analyzer/python/ConfFile_fig.py
+++ Analyzer/python/ConfFile_fig.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-from Configuration.Eras.Era_Phase2C6_cff import Phase2C6
+from Configuration.Eras.Era_Phase2C10_cff import Phase2C10
 
-process = cms.Process("Demo",Phase2C6)
+process = cms.Process('DEMO',Phase2C10)
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.options = cms.untracked.PSet (
@@ -11,28 +11,23 @@ process.options = cms.untracked.PSet (
     numberOfStreams = cms.untracked.uint32(8)
 )
 
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D47Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D47_cff')
+
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T15', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
-
-process.load("Geometry.ForwardCommonData.hfnoseXML_cfi")
-process.load("Geometry.ForwardCommonData.hfnoseParametersInitialization_cfi")
-process.load("Geometry.ForwardCommonData.hfnoseNumberingInitialization_cfi")
-process.load("Geometry.CaloEventSetup.HFNoseTopology_cfi")
-process.load("Geometry.ForwardGeometry.HFNoseGeometryESProducer_cfi")
 
 #if hasattr(process,'MessageLogger'):
 #process.MessageLogger.categories.append('HGCalGeom')
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-     'file:/tmp/dalfonso/Pion_Pt10_Step3.root'
-
+     'file:/eos/user/d/dalfonso/HGCnose/11_1_pre7/step3_D47_PionPt20.root'
     )
 )
 
@@ -42,6 +37,7 @@ process.comparisonPlots = cms.EDAnalyzer("GenAnalyzer")
 #                                         GenJetTag = cms.InputTag("genJets")
 #)
 
-process.TFileService = cms.Service('TFileService', fileName = cms.string('test_Pion_Pt10_Step3.root'))
+
+process.TFileService = cms.Service('TFileService', fileName = cms.string('plots_recHits_D47_PionPt20.root'))
 
 process.p = cms.Path(process.comparisonPlots)
